c3777a5df4d35a7e6cb784f149b2dde50cc2f346
BjornMelgaard <melgaardbjorn@gmail.com>
2017-09-02 18:35:52 +0300

    lint error fixed, document.tsx still ignored

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,23 @@
+import * as React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default function CustomDocument(): JSX.Element {
-  const sheet = new ServerStyleSheet()
-  const main = sheet.collectStyles(<Main />)
-  const styleTags = sheet.getStyleElement()
-  return (
-    <html>
-      <Head>
-        <title>My page</title>
-        {styleTags}
-      </Head>
-      <body>
-        <div className="root">{main}</div>
-        <NextScript />
-      </body>
-    </html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+          {styleTags}
+        </Head>
+        <body>
+          <div className="root">{main}</div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
 }
